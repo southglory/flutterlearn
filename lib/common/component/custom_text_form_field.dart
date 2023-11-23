cbb63664638b1914e95306711b96a2bf5a78306e
southglory <devramyun@gmail.com>
2023-11-23 19:07:10 +0900

    contentPadding, hintText

--- lib/common/component/custom_text_form_field.dart
+++ lib/common/component/custom_text_form_field.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutterlearn/common/const/colors.dart';
 
 class CustomTextFormField extends StatelessWidget {
   const CustomTextFormField({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: '이메일을 입력해주세요',
+      )
+    );
   }
 }
